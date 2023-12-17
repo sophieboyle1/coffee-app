91a381edf89de5274fee722546eab4731b4ab8a3
Sophie Boyle <sophieboyle@Sophies-MacBook-Air.local>
2023-12-17 12:01:06 +0000

    added images, styled nav bar

--- src/App.js
+++ src/App.js
@@ -6,35 +6,45 @@ import Header from './components/header';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Create from './components/create';
 import Read from './components/read';
 import Edit from './components/edit';
+import Logo from './images/logo.png';
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-       <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="/">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/create">Create</Nav.Link>
-            <Nav.Link href="/read">Read</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <Routes>
-        <Route path='/' element={<Content></Content>}></Route>
-        <Route path='/read' element={<Read></Read>}></Route>
-        <Route path='/create' element={<Create></Create>}></Route>
-        <Route path='/edit/:id' element={<Edit></Edit>}></Route>
-      </Routes>
-      {/* <Header></Header>
-      <Content></Content>
-      <Footer /> */}
-    </div>
+      <div className="App">
+        <Navbar bg="light" variant="light" expand="lg" className="px-3">
+          <Container>
+            <Navbar.Brand href="/">
+              <img
+                src={Logo}
+                width="auto"
+                height="30"
+                className="d-inline-block align-top"
+                alt="Your Brand Logo"
+              />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/create">Add Coffee</Nav.Link>
+                <Nav.Link href="/read">Coffee</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Routes>
+          <Route path='/' element={<Content />} />
+          <Route path='/read' element={<Read />} />
+          <Route path='/create' element={<Create />} />
+          <Route path='/edit/:id' element={<Edit></Edit>}></Route>
+        </Routes>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
