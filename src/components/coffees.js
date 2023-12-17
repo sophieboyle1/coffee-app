79280a421861fbb0f054623a40f228e0ff5d31a1
Sophie Boyle <sophieboyle@Sophies-MacBook-Air.local>
2023-12-17 15:31:45 +0000

    update delete functionaly to handle errors

--- src/components/coffees.js
+++ src/components/coffees.js
@@ -3,24 +3,35 @@ import CoffeeItem from "./coffeeItems";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-function Coffees(props){
-    if (!Array.isArray(props.myCoffees)) {
-        // If `myCoffees` is not an array, return a fallback UI or null
-        return <div>No coffees available</div>;
+function Coffees(props) {
+    // Function to call after a coffee is deleted
+    const handleDelete = (coffeeId) => {
+        // Ensure myCoffees is an array before filtering
+        if (Array.isArray(props.myCoffees)) {
+            const updatedCoffees = props.myCoffees.filter(coffee => coffee._id !== coffeeId);
+            props.setMyCoffees(updatedCoffees);
+        }
+    };
+
+    // Check if myCoffees is not an array or is empty
+    if (!Array.isArray(props.myCoffees) || props.myCoffees.length === 0) {
+        return <h2>No coffees to display, Add your favourite Coffee!</h2>;
     }
 
     return (
         <div className="custom-section-2">
-        <h2>Favourite Coffee</h2>
-        <Row className="d-flex align-items-stretch">
-            {props.myCoffees.map((coffee) => {
-                return (
+            <h2>Favourite Coffee</h2>
+            <Row className="d-flex align-items-stretch">
+                {props.myCoffees.map((coffee) => (
                     <Col md={4} sm={6} xs={12} key={coffee._id} className="d-flex">
-                        <CoffeeItem myCoffee={coffee} ReloadData={props.ReloadData} />
+                        <CoffeeItem 
+                            myCoffee={coffee} 
+                            onDelete={() => handleDelete(coffee._id)} 
+                            ReloadData={props.ReloadData} 
+                        />
                     </Col>
-                );
-            })}
-        </Row>
+                ))}
+            </Row>
         </div>
     );
 }
